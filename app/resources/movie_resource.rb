98fcf4562d6ee9f47338d4b920ed2ff2089fc99e
Raghu Betina <raghu.betina@chicagobooth.edu>
2021-08-09 22:53:29 +0000

    Generate graphiti resources for movies

---
+++ app/resources/movie_resource.rb
@@ -0,0 +1,12 @@
+class MovieResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :title, :string
+  attribute :director_id, :integer
+
+  # Direct associations
+
+  # Indirect associations
+
+end
